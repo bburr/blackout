a98c70c099672f12f028959973557ce4694ca9a5
Bryan Burr <burr.bryan@gmail.com>
2022-08-09 22:25:16 -0400

    silently handle num cards dealt = 52 for now, intend to handle properly with per-round trump card config

--- app/Jobs/DealForRound.php
+++ app/Jobs/DealForRound.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Exceptions\CardShoeIsEmptyException;
 use App\State\GameState;
 use App\State\PlayerState;
 
@@ -21,7 +22,17 @@ class DealForRound
         }
 
         if ($this->gameState->getCurrentRound()->shouldDrawTrumpCard()) {
-            $this->gameState->getCurrentRound()->setTrumpCard($this->gameState->getCardShoeState()->dealCardOut());
+            try {
+                $this->gameState->getCurrentRound()->setTrumpCard($this->gameState->getCardShoeState()->dealCardOut());
+            }
+            catch (CardShoeIsEmptyException $e) {
+                // todo handle this situation beforehand when configuring trump/no trump for each round before game start
+                \Log::debug('CardShoeIsEmpty', [
+                    'num_tricks' => $this->gameState->getCurrentRound()->getNumTricks(),
+                    'num_players' => $this->gameState->getPlayers()->count(),
+                    'round_number' => $this->gameState->getCurrentRound()->getRoundNumber(),
+                ]);
+            }
         }
     }
 }
