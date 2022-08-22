536117953d83a2b237c0e9cc72e1ed551525ce56
Bryan Burr <burr.bryan@gmail.com>
2022-08-22 18:58:32 -0400

    misc config updates

--- vite.config.ts
+++ vite.config.ts
@@ -16,9 +16,7 @@ export default defineConfig({
         },
     },
     server: {
-        hmr:  {
-            host: '127.0.0.1'
-        },
+        host: '127.0.0.1',
         watch: {
             usePolling: true
         }
